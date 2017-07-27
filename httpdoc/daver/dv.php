b73d32df1d23f324c389afef34444743ae4131a6
David Rosendahl <daver@mindfireinc.com>
2017-07-27 23:03:51 +0000

    Few updates to POC content editing

--- httpdoc/daver/dv.php
+++ httpdoc/daver/dv.php
@@ -5,17 +5,38 @@
 
 error_reporting(E_ALL ^ E_NOTICE);
 
-$page = ($_REQUEST['page']);
+$page   = ($_REQUEST['page']);
+$action = ($_REQUEST['action']);
 
 if ($page) {
+    if ($action) {
+        doAction($_POST);
+    }
     echo showPage($page);
-} else {
+    } else {
     echo showPage('homePage');
 }
 
 function showPage($page) {
+    $vars       = file_get_contents("credentials.json"); // Get this user's information.  Later this is in DB
+    $vars       = json_decode($vars, true);
+    
+    // Create array $vars_array that has the {{var}} = value stuff we need to inject within our UI tempaltes
+    foreach ($vars as $k => $v) {
+        $vars_array['{{'.$k.'}}'] = $v;
+        unset($vars_array[$k]);
+    }
+
+    // Get our wrapper html, insert the body into it, and then repace all our vars using vars_array
     $wrapper    = file_get_contents('wrapper.html');
-    $body = file_exists($page.'.html') ? file_get_contents($page.'.html') : "<h2>Shoot.  It appears Da Vinci farted. Turn away quickly.</h2>";
+    $body       = file_exists($page.'.html') ? file_get_contents($page.'.html') : "<h2>Shoot.  It appears Da Vinci farted. Turn away quickly.</h2>";
     $html       = str_replace("{{content}}",$body,$wrapper);
+    $html       = str_replace(array_keys($vars_array), $vars_array, $html);
     return $html;
+}
+
+function doAction($POST) {
+    if ($POST['action'] == "saveProfileSettings") {
+        file_put_contents('credentials.json', json_encode($POST));
+    }
 }
