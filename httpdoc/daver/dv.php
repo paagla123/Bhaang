9c98c147fc7bf3fee4767beec97b22cd17f5843d
David Rosendahl <daver@mindfireinc.com>
2017-07-27 19:07:22 +0000

    Small changes to be quicker in mock up for Kushy

--- httpdoc/daver/dv.php
+++ httpdoc/daver/dv.php
@@ -15,7 +15,7 @@ if ($page) {
 
 function showPage($page) {
     $wrapper    = file_get_contents('wrapper.html');
-    $body       = file_get_contents($page.'.html');
+    $body = file_exists($page.'.html') ? file_get_contents($page.'.html') : "<h2>Shoot.  It appears Da Vinci farted. Turn away quickly.</h2>";
     $html       = str_replace("{{content}}",$body,$wrapper);
     return $html;
 }
